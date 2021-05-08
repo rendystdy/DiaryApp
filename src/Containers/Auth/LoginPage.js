1992b1f73daacb9c6abd69f5b2f64e8f55496ff0
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-08 20:18:35 +0700

    Feat: add LoginPage

---
+++ src/Containers/Auth/LoginPage.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import { useTheme } from '../../Theme'
+import { InputText, Gap, Button, SignUp } from '../../Components'
+import { navigateAndSimpleReset } from '../../Navigators/Root'
+
+const LoginPage = () => {
+  const { Layout, Images, Fonts, Colors } = useTheme()
+  return (
+    <View style={[Layout.fill, styles.container]}>
+      <ScrollView style={Layout.fill} showsVerticalScrollIndicator={false}>
+        <View style={Layout.alignItemsCenter}>
+          <Image
+            source={Images.logo}
+            style={{ width: 180, height: 180 }}
+            resizeMode="contain"
+          />
+          <Text style={[Fonts.titleSmall]}>Login</Text>
+        </View>
+        <Gap height={39} />
+        <InputText
+          title="E-mail"
+          onChange={() => {}}
+          placeholder="yourname@gmail.com"
+          keyboardType="email-address"
+        />
+        <Gap height={14} />
+        <InputText
+          title="Password"
+          secureTextEntry
+          onChange={() => {}}
+          placeholder="your password"
+          keyboardType="default"
+        />
+        <Gap height={65} />
+        <Button
+          bgColor={Colors.primary}
+          text="Login"
+          onPress={() => {
+            navigateAndSimpleReset('HomePage')
+          }}
+        />
+        <Gap height={39} />
+        <SignUp />
+      </ScrollView>
+    </View>
+  )
+}
+
+export default LoginPage
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: Colors.pageBackground, paddingHorizontal: 20 },
+})
--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,2 +1,4 @@
 export { default as IndexExampleContainer } from './Example/Index'
 export { default as IndexStartupContainer } from './Startup/Index'
+export { default as LoginPage } from './Auth/LoginPage'
+export { default as HomePage } from './HomePage/index'
