9f749fdd7ac522db5fc3b1e80bd277a5415b8396
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-08 20:21:55 +0700

    Feat: modify page StartUp

--- src/Containers/Startup/Index.js
+++ src/Containers/Startup/Index.js
@@ -2,26 +2,50 @@ import React, { useEffect } from 'react'
 import { ActivityIndicator, View, Text } from 'react-native'
 import { useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next'
-import { useTheme } from '@/Theme'
-import InitStartup from '@/Store/Startup/Init'
-import { Brand } from '@/Components'
+import { useTheme } from '../../Theme'
+import InitStartup from '../../Store/Startup/Init'
+import { Brand, Button } from '../../Components'
+import { navigate } from '../../Navigators/Root'
 
 const IndexStartupContainer = () => {
-  const { Layout, Gutters, Fonts } = useTheme()
+  const { Layout, Gutters, Fonts, Colors } = useTheme()
 
   const { t } = useTranslation()
 
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(InitStartup.action())
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(InitStartup.action())
+  // }, [dispatch])
 
   return (
-    <View style={[Layout.fill, Layout.colCenter]}>
-      <Brand />
-      <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
-      <Text style={Fonts.textCenter}>{t('welcome')}</Text>
+    <View style={[Layout.fill, { backgroundColor: Colors.pageBackground }]}>
+      <View
+        style={[
+          Layout.fill,
+          Layout.alignItemsCenter,
+          Layout.justifyContentBetween,
+          Gutters.largeVPadding,
+          Gutters.largeHPadding,
+        ]}
+      >
+        <Brand />
+        <View>
+          <Text style={[Fonts.textCenter, Fonts.titleSmall]}>
+            {t('welcome')}
+          </Text>
+          <Text style={[Fonts.textCenter, Fonts.textSmall]}>
+            {t('description')}
+          </Text>
+        </View>
+        <Button
+          text="Getting Started"
+          onPress={() => {
+            navigate('LoginPage')
+          }}
+          width={225}
+        />
+      </View>
     </View>
   )
 }
