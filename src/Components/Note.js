3f434632f9ee603a074d5729e1592bd16c6c1bf9
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-08 20:15:54 +0700

    Feat: add Note component

---
+++ src/Components/Note.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import moment from 'moment'
+
+const Note = ({ data }) => {
+  return (
+    <View style={styles.container}>
+      <Text>{data.title}</Text>
+      <Text style={styles.dateTime}>
+        {moment(new Date()).format('MMM d, YYYY')}
+      </Text>
+    </View>
+  )
+}
+
+export default Note
+
+const styles = StyleSheet.create({
+  container: {
+    width: width / 2 - 20,
+    backgroundColor: '#FFAB91',
+    marginRight: 8,
+    marginBottom: 8,
+    borderRadius: 6,
+    padding: 10,
+  },
+  dateTime: { fontSize: 10, color: 'rgba(0, 0, 0, 0.5)' },
+})
