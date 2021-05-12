2fe508e496a8e57b008acc5417a1d4df1536d4d8
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-12 19:31:45 +0700

    Refactor: modify Note component

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,30 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native'
 import moment from 'moment'
 
 const { width } = Dimensions.get('screen')
 
-const Note = ({ data }) => {
+const backgroundColor = [
+  '#FFCC80',
+  '#FFAB91',
+  '#E6EE9B',
+  '#80DEEA',
+  '#CF93D9',
+  '#F490B1',
+]
+
+const Note = ({ data, onPress }) => {
+  const myRandomColor =
+    backgroundColor[Math.floor(Math.random() * backgroundColor.length)]
+
   return (
-    <View style={styles.container}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={styles.container(myRandomColor)}
+    >
       <Text>{data.title}</Text>
       <Text style={styles.dateTime}>
-        {moment(new Date()).format('MMM d, YYYY')}
+        {moment(data.date).format('MMM d, YYYY')}
       </Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
 export default Note
 
 const styles = StyleSheet.create({
-  container: {
+  container: backColor => ({
     width: width / 2 - 20,
-    backgroundColor: '#FFAB91',
+    backgroundColor: backColor,
     marginRight: 8,
     marginBottom: 8,
     borderRadius: 6,
     padding: 10,
-  },
+  }),
   dateTime: { fontSize: 10, color: 'rgba(0, 0, 0, 0.5)' },
 })
