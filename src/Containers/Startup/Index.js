8ff854eab26e009a095339ca6c0d6fe8afd579da
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-12 19:38:28 +0700

    Feat: modify Startup page

--- src/Containers/Startup/Index.js
+++ src/Containers/Startup/Index.js
@@ -1,53 +1,87 @@
-import React, { useEffect } from 'react'
-import { ActivityIndicator, View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  ActivityIndicatorBase,
+} from 'react-native'
 import { useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { useTheme } from '../../Theme'
 import InitStartup from '../../Store/Startup/Init'
 import { Brand, Button } from '../../Components'
 import { navigate } from '../../Navigators/Root'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { connect } from 'react-redux'
 
-const IndexStartupContainer = () => {
+const IndexStartupContainer = props => {
   const { Layout, Gutters, Fonts, Colors } = useTheme()
+  const [isLogin, setIsLogin] = useState(true)
 
   const { t } = useTranslation()
 
-  // const dispatch = useDispatch()
+  useEffect(() => {
+    checkLoggin()
+  }, [])
 
-  // useEffect(() => {
-  //   dispatch(InitStartup.action())
-  // }, [dispatch])
+  const checkLoggin = async () => {
+    try {
+      const isLogin = await AsyncStorage.getItem('isLogin')
+      if (isLogin === 'success') {
+        setTimeout(() => {
+          setIsLogin(false)
+          navigate('HomePage')
+        }, 2000)
+      } else {
+        setTimeout(() => {
+          setIsLogin(false)
+        }, 2000)
+      }
+    } catch (e) {}
+  }
 
-  return (
-    <View style={[Layout.fill, { backgroundColor: Colors.pageBackground }]}>
-      <View
-        style={[
-          Layout.fill,
-          Layout.alignItemsCenter,
-          Layout.justifyContentBetween,
-          Gutters.largeVPadding,
-          Gutters.largeHPadding,
-        ]}
-      >
-        <Brand />
-        <View>
-          <Text style={[Fonts.textCenter, Fonts.titleSmall]}>
-            {t('welcome')}
-          </Text>
-          <Text style={[Fonts.textCenter, Fonts.textSmall]}>
-            {t('description')}
-          </Text>
-        </View>
-        <Button
-          text="Getting Started"
-          onPress={() => {
-            navigate('LoginPage')
-          }}
-          width={225}
-        />
+  if (isLogin) {
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <ActivityIndicator size={22} color="red" />
       </View>
-    </View>
-  )
+    )
+  } else {
+    return (
+      <View style={[Layout.fill, { backgroundColor: Colors.pageBackground }]}>
+        <View
+          style={[
+            Layout.fill,
+            Layout.alignItemsCenter,
+            Layout.justifyContentBetween,
+            Gutters.largeVPadding,
+            Gutters.largeHPadding,
+          ]}
+        >
+          <Brand />
+          <View>
+            <Text style={[Fonts.textCenter, Fonts.titleSmall]}>
+              {t('welcome')}
+            </Text>
+            <Text style={[Fonts.textCenter, Fonts.textSmall]}>
+              {t('description')}
+            </Text>
+          </View>
+          <Button
+            text="Getting Started"
+            onPress={() => {
+              navigate('LoginPage')
+            }}
+            width={225}
+          />
+        </View>
+      </View>
+    )
+  }
 }
 
-export default IndexStartupContainer
+const mapStateToProps = state => ({
+  isLogin: state.login.isLogin,
+})
+
+export default connect(mapStateToProps, null)(IndexStartupContainer)
